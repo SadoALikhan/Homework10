bb67273e7ad46694d68f1c92a66d454ee58eba3f
SadoALikhan <khan.ali.99.sado@mail.ru>
2024-03-14 10:17:41 +0300

    Homework10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1 и 2:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("1)Ф.И.О. сотрудника — " + fullName + ".");
+        System.out.println("2)Данные Ф. И. О. сотрудника для заполнения отчета — " +
+                fullName.toUpperCase() + ".");
+
+        System.out.println("Задача 3:");
+        String fullNameNew = "Иванов Семён Семёнович";
+        String fullNameNewWithout = fullNameNew.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameNewWithout + ".");
+
+
 
     }
 }
